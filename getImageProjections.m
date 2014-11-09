63cf3095419c33569aa87b805e3df51f304bc2f2
Jherez Taylor <jherez.taylor@gmail.com>
2014-11-09 16:02:34 -0400

    Testing image projections

--- getImageProjections.m
+++ getImageProjections.m
@@ -1,11 +1,15 @@
-function[horizontalProjection, verticalProjection] = getImageProjections(image)
+function[horizontalProjection, verticalProjection, baseline] = getImageProjections(image)
 %Vertical and Horizontal Projection
 
 
 %image = getImage();
 
-verticalProjection = sum(image, 1); %vertical projection
-horizontalProjection = sum(image, 2); %horizontal projection
+verticalProjection = sum(image, 2); %vertical projection
+horizontalProjection = sum(image, 1); %horizontal projection
+horizontalProjection = flipud(horizontalProjection);
+verticalProjection = fliplr(verticalProjection);
+[val, idx] = min(horizontalProjection);
+baseline = [val, idx];
 
 %plot(verticalProjection);
 %plot(horizontalProjection);
