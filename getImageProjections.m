73cd4622f15fa72c545ac27ee30f4b539a5feaab
dillonbhola <dillonbhola@gmail.com>
2014-11-09 16:36:15 -0400

    h&v projections fix

--- getImageProjections.m
+++ getImageProjections.m
@@ -4,10 +4,10 @@ function[horizontalProjection, verticalProjection, baseline] = getImageProjectio
 
 %image = getImage();
 
-verticalProjection = sum(image, 2); %vertical projection
-horizontalProjection = sum(image, 1); %horizontal projection
-horizontalProjection = flipud(horizontalProjection);
-verticalProjection = fliplr(verticalProjection);
+verticalProjection = sum(image, 1); %vertical projection
+horizontalProjection = sum(image, 2); %horizontal projection
+%horizontalProjection = flipud(horizontalProjection);
+%verticalProjection = fliplr(verticalProjection);
 [val, idx] = min(horizontalProjection);
 baseline = [val, idx];
 
